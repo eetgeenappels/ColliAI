f317fc5b39c961e943dbd40403f0d83e64a4724c
eetgeenappels <62078218+eetgeenappels@users.noreply.github.com>
2023-10-25 23:44:27 +0200

    llama-cpp-python bindings instead of pyllamacpp

--- textgen/interference.py
+++ textgen/interference.py
@@ -1,6 +1,5 @@
-from pyllamacpp.model import Model
+from llama_cpp import Llama
 import json
-from tqdm import tqdm
 import credentials
 
 class ConversationalAI:
@@ -8,53 +7,25 @@ class ConversationalAI:
         self.memory = None
 
         with open("character.json", 'r')as file:
-             prompt_context = json.loads(file.read())["character_description"]
+            self.prompt_context = json.loads(file.read())["character_description"]
 
         prompt_prefix = ""
         prompt_suffix = "\n### Colli"
                 
         # load model
-        self.model = Model(model_path=credentials.model,
-              prompt_prefix=prompt_prefix,
-              prompt_suffix=prompt_suffix,
-              use_mlock = True)
+        self.model = Llama(model_path=credentials.model, use_mlock=True, n_gpu_layers=-1,prompt_prefix=prompt_prefix,
+              prompt_suffix=prompt_suffix)
         
 
     def generate_response(self, user_input,memory_prompt = None, api_prompt = None):
         # Join messages
-        conversation_history_input = "".join(user_input)
+        conversation_history_input = "".join(self.prompt_context)
 
         if memory_prompt != None:
             conversation_history_input = "\n" + conversation_history_input[1:].join(memory_prompt) + conversation_history_input[-1:]
         if api_prompt != None:
             conversation_history_input = "\n" + conversation_history_input[1:] + api_prompt + conversation_history_input[-1:]
-
-
-        generated_response = ""
-
-        # Wrap the loop with tqdm
-        for token in tqdm(self.model.generate(conversation_history_input,
-                                            antiprompt='### You',
-                                            n_threads=6,
-                                            n_predict=40),
-                        desc='Generating response',
-                        unit='token'):
-            generated_response += token
-            if len(generated_response) >= 4:
-                continue
-
-            if generated_response[-3:] == "###":
-                # remove Colli from document
-                generated_response = generated_response[3:]
-                break
         
-            if len(generated_response) >= 3:
-                continue
+        conversation_history_input = conversation_history_input.join(user_input)
 
-            # check if last 6 characters are COlli
-            if generated_response[-3:] == "###":
-                # remove Colli from document
-                generated_response = generated_response[3:]
-                break
-
-        return generated_response
+        return self.model(conversation_history_input, stop=["\n","###"],repeat_penalty=1.5,echo=True)["choices"][0]["text"][len(conversation_history_input):]
