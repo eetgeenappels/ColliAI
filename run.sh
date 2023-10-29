d79d0413a9cf37565e76d59b22ad5e3c2c091cea
eetgeenappels <62078218+eetgeenappels@users.noreply.github.com>
2023-10-29 18:27:25 +0100

    little folder change (needs testing)

---
+++ run.sh
@@ -0,0 +1,5 @@
+export PYTHONDONTWRITEBYTECODE=1
+source env/bin/activate
+cd colliai
+ls
+python colli_ai.py --config_file ../config.json
---
+++ setup.sh
@@ -0,0 +1,2 @@
+git clone https://github.com/eetgeenappels/ColliAI
+./setup_env.sh
